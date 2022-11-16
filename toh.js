dee771454911f61ccd312c65a050827339bf50b0
chocabloc <46107414+chocabloc@users.noreply.github.com>
2022-11-16 10:57:06 +0530

    Update toh.js

--- toh.js
+++ toh.js
@@ -2,12 +2,11 @@ var ctx;
 var width, height;
 var pegWidth = 8;
 var diskHeight = 30;
-var stage;
+var stage, stageOriginal;
 
 function movePegs(pegs, from, to, num, moves) {
     if (num == 0)
         return;
-    s
     var tmpeg = 2 * pegs - 3 - from - to;
     movePegs(pegs, from, tmpeg, num - 1, moves);
     moves.push([ from, to ]);
@@ -23,23 +22,35 @@ function getSolution(pegs, disks) {
     movePegs(pegs, 0, pegs - 1, disks - reservePegs, moves)
     for (var i = reservePegs; i >= 1; i--)
         moves.push([i, pegs - 1]);
-
-    for (var i = 0; i < moves.length; i++)
-        console.log(moves[i][0] + "->" + moves[i][1]);
     return moves;
 }
 
 function drawStage(s) {
-    if (s.won) {
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "#ffffff";
+
+    if (s.autosolve) {
+        ctx.font = '40px sans';
+        if (s.won)
+            ctx.fillText("Done", width/2 - 30, 100);
+        else
+            ctx.fillText("Auto-solving...", width/2 - 100, 100);
+    } else if (s.won) {
         ctx.font = '48px sans';
-        ctx.fillText("You Won", width / 2 - 100, 80);
+        ctx.fillText("You Won", width / 2 - 100, 100);
     }
+
+    ctx.font = '22px sans';
+    ctx.fillText("Moves Taken: " + s.moves, 30, 35);
 
     var space = width / (s.pegs + 1);
 
     for (var i = 0; i < s.pegs; i++) {
         var pc = space * (i + 1);
         ctx.beginPath();
+        ctx.fillStyle = "#ffffff";
         ctx.rect(pc - (pegWidth / 2), height - 350, pegWidth, 330);
         ctx.fill();
         ctx.closePath();
@@ -47,31 +58,55 @@ function drawStage(s) {
         for (var j = 0; j < s.data[i].length; j++){
             var diskwidth = ((space - 15) / s.disks) * s.data[i][j];
             ctx.beginPath();
+            ctx.strokeStyle = "#000000";
+            ctx.fillStyle = s.colors[s.data[i][j] - 1];
             ctx.rect(pc - (diskwidth / 2), height - 20 - diskHeight * (j + 1), diskwidth, diskHeight);
-            if (s.selected && s.sp == i && s.sd == j) {
-                ctx.stroke();
-            } else {
-                ctx.fill();
-            }
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+
+            ctx.beginPath();
+            if (s.selected && s.sp == i && s.sd == j)
+                ctx.fillStyle = ctx.createPattern(document.getElementById("txt"), "repeat");
+            ctx.rect(pc - (diskwidth / 2), height - 20 - diskHeight * (j + 1), diskwidth, diskHeight);
+            ctx.fill();
+            ctx.stroke();
             ctx.closePath();
         }
     }
 }
 
 function init() {
-    stage = { data: new Array(3), pegs: 3, disks: 3, selected: false, sp: 0, sd: 0, won: false };
+    var npegs = parseInt(prompt("Enter number of Pegs (3-6):"));
+    while (isNaN(npegs) || npegs < 3 || npegs > 6)
+        npegs = parseInt(prompt("Invalid Input. Enter number of Pegs (3-6):"));
+
+    var ndisks = parseInt(prompt("Enter number of Disks (1-6):"));
+    while (isNaN(ndisks) || ndisks < 1 || ndisks > 6)
+        ndisks = parseInt(prompt("Invalid Input. Enter number of Disks (1-6):"));
+
+    stage = { data: new Array(npegs), pegs: npegs, disks: ndisks, selected: false, sp: 0, sd: 0, won: false, moves: 0,
+              autosolve: false, colors: new Array(0) };
     for (var i = 0; i < stage.pegs; i++)
         stage.data[i] = new Array(0);
     for (var i = stage.disks; i >= 1; i--)
         stage.data[0].push(i);
+    for (var i = 0; i < ndisks; i++) {
+        var r = Math.floor(200 + Math.random() * 56),
+            g = Math.floor(200 + Math.random() * 56),
+            b = Math.floor(200 + Math.random() * 56);
+        stage.colors.push("rgb(" + r + ", " + g + ", " + b + ")");
+        console.log(stage.colors[i]);
+    }
     
+    stageOriginal = JSON.parse(JSON.stringify(stage));
     cnv = document.getElementById("cnv");
     width = cnv.width = document.body.clientWidth;
     height = cnv.height = document.body.clientHeight;
     ctx = cnv.getContext("2d");
 
     cnv.addEventListener('click', function (event) {
-        if (stage.won)
+        if (stage.won || stage.autosolve)
             return;
         
         var x = event.pageX, y = event.pageY;
@@ -97,6 +132,7 @@ function init() {
                     stage.data[peg].push(stage.data[stage.sp][stage.sd]);
                     stage.data[stage.sp].pop();
                     stage.selected = false;
+                    stage.moves++;
                 }
             }
         }
@@ -111,9 +147,46 @@ function init() {
         if (stage.data[stage.pegs - 1].length == stage.disks)
             stage.won = true;
         
-        ctx.clearRect(0, 0, width, height);
         drawStage(stage);
     });
 
     drawStage(stage);
+}
+
+function reset() {
+    stage = JSON.parse(JSON.stringify(stageOriginal));
+    console.log(JSON.stringify(stageOriginal));
+    drawStage(stage);
+}
+
+function newgame() {
+    location.reload();
+}
+
+function autosolve_step(moves, i) {
+    if (!stage.autosolve)
+        return;
+
+    console.log(moves[i][0] + "->" + moves[i][1]);
+    stage.data[moves[i][1]].push(stage.data[moves[i][0]].pop());
+    stage.moves++;
+    drawStage(stage);
+
+    if (i + 1 == moves.length) {
+        stage.won = true;
+        drawStage(stage);
+        return;
+    }
+    setTimeout(function() {
+        autosolve_step(moves, i + 1)
+    }, 1000);
+}
+
+function autosolve() {
+    reset();
+    stage.autosolve = true;
+    drawStage(stage);
+
+    moves = getSolution(stage.pegs, stage.disks);
+    autosolve_step(moves, 0);
 }
